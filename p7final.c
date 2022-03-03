071873ab169c7353ea3073b64c29c57b5575f279
harishrockybhai <95605196+harishrockybhai@users.noreply.github.com>
2022-03-03 08:49:23 +0000

    P7 FINAL.C

--- p7final.c
+++ p7final.c
@@ -1,1 +1,21 @@
+#include<stdio.h>
+typedef struct _fraction
+{
+int num,den;
+}Fraction;
 
+int gcd(int a, int b)
+{
+  int t;
+  while(b!=0)
+    {
+      t=b;
+      b=a%b;
+      a=t;
+    }
+  return a;
+}
+ int main()
+{
+  printf("%d",gcd(16,24));
+}
